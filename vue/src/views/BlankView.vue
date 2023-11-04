4e521f8ee30e394500b9800896ab9ebd617d7774
naroop <nrupp815@outlook.com>
2023-11-03 23:45:20 -0500

    primeicons

--- vue/src/views/BlankView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="w-full h-full flex justify-center items-center bg-[var(--surface-ground)]">
-  </div>
-</template>
-
-<script setup lang="ts">
-</script>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-[var(--surface-ground)] px-32 h-full">
+    <top-nav />
+    <div class="flex w-full h-full justify-center gap-1">
+      <div class="flex flex-col border w-2/12">
+        <left-nav />
+      </div>
+      <div class="flex flex-col bg-green-400 w-7/12">
+        <the-timeline />
+      </div>
+      <div class="flex flex-col bg-red-400 w-3/12">
+        <right-nav />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheTimeline from '@/components/TheTimeline.vue';
+import LeftNav from '@/components/LeftNav.vue';
+import RightNav from '@/components/RightNav.vue';
+import TopNav from '@/components/TopNav.vue';
+</script>

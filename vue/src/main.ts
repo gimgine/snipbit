f214dd74c2f7fe9f6d44d5c0480a037060e1b2de
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2023-11-04 11:16:44 -0500

    account creation

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import App from './App.vue';
 import router from './router';
 
@@ -13,4 +14,5 @@ const pinia = createPinia();
 app.use(router);
 app.use(pinia);
 app.use(PrimeVue);
+app.use(ToastService);
 app.mount('#app');

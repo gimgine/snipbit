15580ef94ed2ee053eab7927bb73a9c6686257dc
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2023-11-05 00:28:15 -0500

    update homeview

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -12,6 +12,9 @@
         <right-nav />
       </div>
     </div>
+    <speed-dial :model="items" direction="up" :style="{ right: '1rem', bottom: '1rem' }" :tooltip-options="{ position: 'right', event: 'hover' }" />
+    <create-snippet ref="createSnippet" />
+    <create-post ref="createPost" />
   </div>
 </template>
 
@@ -20,4 +23,30 @@ import TheTimeline from '@/components/TheTimeline.vue';
 import LeftNav from '@/components/LeftNav.vue';
 import RightNav from '@/components/RightNav.vue';
 import TopNav from '@/components/TopNav.vue';
+import SpeedDial from 'primevue/speeddial';
+import { ref } from 'vue';
+import type { MenuItem } from 'primevue/menuitem';
+import CreateSnippet from '@/components/CreateSnippet.vue';
+import CreatePost from '@/components/CreatePost.vue';
+
+const createSnippet = ref({} as InstanceType<typeof CreateSnippet>)
+const createPost = ref({} as InstanceType<typeof CreatePost>)
+const items = ref<Array<MenuItem>>([
+  {
+    label: 'Post',
+    icon: 'pi pi-file-edit',
+    command: () => createPost.value.open()
+  },
+  {
+    label: 'Snippet',
+    icon: 'pi pi-code',
+    command: () => createSnippet.value.open()
+  }
+]);
 </script>
+
+<style>
+.p-speeddial {
+  position: fixed !important;
+}
+</style>
